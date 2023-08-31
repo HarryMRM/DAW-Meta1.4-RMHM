fad50ec36b9e1a36b17e0f367c01eb600a3afa55
HarryMRM <harry.manuel.reynozo.magallanes@uabc.edu.mx>
2023-08-31 10:41:13 -0700

    Version 3

--- clienteDatos.js
+++ clienteDatos.js
@@ -1,12 +1,26 @@
+/*
+	Desarrollo de Aplicaciones Web
+	Meta 1.4 Ejercicio Promesas:Cliente
+	Código por: Harry Manuel Reynozo Magallanes
+*/
+
+/*
+	Este codigo solicita al servicio informacion
+    sobre ciertos procesos, espera el resultado de
+    una promesa mediante then's anidados para evitar
+    problemas de sincronia.
+*/
 const servicio = require("./servicioDatos");
-servicio.getDatos(1,3000,function(){
-    servicio.getDatos(2,500,function(){
-        servicio.getDatos(3,4000,function(){
-            servicio.getDatos(4,700,function(){
-                servicio.getDatos(5,3500,function(){
-                    console.log("Programa Terminado");
-                })
-            })
-        })
-    })
-});
+servicio.getDatos(1,3000).then(function(r){
+    console.log(r);
+    return servicio.getDatos(2,500)
+}).then(function(r){
+    console.log(r);
+    return servicio.getDatos(3,4000)
+}).then(function(r){
+    console.log(r);
+    return servicio.getDatos(4,700)
+}).then(function(r){
+    console.log(r);
+    return servicio.getDatos(5,3500)
+});;
--- servicioDatos.js
+++ servicioDatos.js
@@ -1,11 +1,23 @@
-let finDeTransmisionDeDatos = function (id,callback){
-    console.log("Transferencia ",id," terminada");
-    callback();
-};
+/*
+	Desarrollo de Aplicaciones Web
+	Meta 1.4 Ejercicio Promesas:Servicio
+	Código por: Harry Manuel Reynozo Magallanes
+*/
 
-let obtenDatosDeInternet = function (id,duracion,callback){
-    console.log("Proceso ",id," obteniendo datos de internet");
-    setTimeout(finDeTransmisionDeDatos,duracion,id,callback);
-};
-
+/*
+	Este codigo envia promesas mediante una funcion
+    async, en la que se enviara la promesa resultante
+    al cliente de datos.
+*/
+let obtenDatosDeInternet = function (id,duracion){
+    const promesa = new Promise(async function (resolve){
+        console.log("Proceso "+id+" obteniendo datos de internet");
+        let myPromise = new Promise(function(resolve){
+            setTimeout(function() {resolve("Transferencia "+id+" terminada");},duracion);
+        });
+        let test = await myPromise;
+        resolve(test);
+    })
+    return promesa;
+}
 module.exports.getDatos = obtenDatosDeInternet;
