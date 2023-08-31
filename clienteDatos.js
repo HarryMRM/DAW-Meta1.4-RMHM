b9da201ca685bd7357b4518871a431374829f6b9
HarryMRM <harry.manuel.reynozo.magallanes@uabc.edu.mx>
2023-08-31 01:49:16 -0700

    Version 2

--- clienteDatos.js
+++ clienteDatos.js
@@ -1,6 +1,12 @@
 const servicio = require("./servicioDatos");
-servicio.getDatos(1,3000);
-servicio.getDatos(2,500);
-servicio.getDatos(3,4000);
-servicio.getDatos(4,700);
-servicio.getDatos(5,3500);
+servicio.getDatos(1,3000,function(){
+    servicio.getDatos(2,500,function(){
+        servicio.getDatos(3,4000,function(){
+            servicio.getDatos(4,700,function(){
+                servicio.getDatos(5,3500,function(){
+                    console.log("Programa Terminado");
+                })
+            })
+        })
+    })
+});
